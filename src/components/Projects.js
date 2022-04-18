320148aab8ebc3027af9a8d999b727d4eae387f4
pooniena2 <anhtuan.pham@ucalgary.ca>
2022-04-18 15:08:53 -0600

    fix animation bug

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,14 +3,6 @@ import "aos/dist/aos.css";
 import Aos from "aos";
 import ProjectData from "../data/ProjectData";
 function Projects() {
-    useEffect(() => {
-      Aos.init(
-        {
-          once: true,
-        },
-        []
-      );
-    });
   return (
     <div className="max-w-full mx-auto py-12 min-h-screen" id="skill">
       <p className="text-2xl text-black md:text-4xl text-center font-serif">
@@ -20,23 +12,10 @@ function Projects() {
         <div class="items-stretch -mx-4">
       <div className="flex flex-wrap justify-center pt-2  w-full">
         {ProjectData.map(function (project, i) {
-          var fadePosition = "fade-right"
-          if (i % 4 === 1) {
-            fadePosition = "fade-down";
-          } else if (i % 4 === 2) {
-            fadePosition = "fade-left";
-          } else if (i % 4 === 3) {
-            fadePosition = "fade-up";
-          }
-          
-        
-          
           return (
             <div className="hover:scale-105 h-full">
               <div
                 className="p-10"
-                data-aos={fadePosition}
-                data-aos-duration="1000"
                 key={i}
               >
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
